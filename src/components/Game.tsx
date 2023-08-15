15cdb770b70e7ff656845838929359367b5fe1df
Micah Ehrlich <micahehrlich@gmail.com>
2023-08-16 01:25:45 +0300

    added GameOver + fixed self collision + added score

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const GAME_SPEED = 200;
 
 type SnakePart = {
-    x:number;
-    y:number;
+    x: number;
+    y: number;
 }
 
 const enum DIRECTION {
@@ -27,7 +27,7 @@ const Food = styled.div<{ $left: number; $top: number }>`
     top: ${(props: { $top: number; }) => props.$top * 50}px;
 `;
 
-const SnakePart = styled.div<{part: {x: number, y:number}}>`
+const SnakePart = styled.div<{ part: { x: number, y: number } }>`
     position: absolute;
     top: ${props => props.part.y * 50}px;
     left: ${props => props.part.x * 50}px;
@@ -37,9 +37,26 @@ const SnakePart = styled.div<{part: {x: number, y:number}}>`
     border: 2px solid #333;
 `;
 
+const ScoreBoard = styled.div`
+    position: absolute;
+    top: 50px; 
+    left: 100px;
+    font-weight: 500;
+    font-size: 30px;
+    color: yellow;
+`;
+
+const GameOver = styled.div`
+    font-weight: 600;
+    font-size: 70px;
+    color: red;
+    top: 50%;
+    left: 50%;
+`;
+
 const generateRandomPosition = () => {
-    const x = Math.floor(Math.random() * 10);
-    const y = Math.floor(Math.random() * 10);
+    const x = Math.floor(Math.random() * 10 + 2);
+    const y = Math.floor(Math.random() * 10 + 2);
 
     return { x, y };
 }
@@ -48,46 +65,45 @@ export const Game = () => {
     const [direction, setDirection] = useState(DIRECTION.UP);
     const [left, setLeft] = useState(200);
     const [bottom, setBottom] = useState(200);
-
-
     const [snakeParts, setSnakeParts] = useState([{ x: 13, y: 13 }]);
     const [food, setFood] = useState(generateRandomPosition());
-
-    // console.log(`food: {x , y}:  ${food.x} ${food.y}`)
-
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            switch (direction) {
-                default:
-                case DIRECTION.UP: setBottom((bottom) => bottom + 50); break;
-                case DIRECTION.DOWN: setBottom((bottom) => bottom - 50); break;
-                case DIRECTION.LEFT: setLeft((left) => left - 50); break;
-                case DIRECTION.RIGHT: setLeft((left) => left + 50); break;
-            }
+            if (!gameOver) {
+                switch (direction) {
+                    default:
+                    case DIRECTION.UP: setBottom((bottom) => bottom + 50); break;
+                    case DIRECTION.DOWN: setBottom((bottom) => bottom - 50); break;
+                    case DIRECTION.LEFT: setLeft((left) => left - 50); break;
+                    case DIRECTION.RIGHT: setLeft((left) => left + 50); break;
+                }
 
-            const head = { ...snakeParts[0] };
-            switch (direction) {
-                case DIRECTION.UP:
-                    head.y--;
-                    break;
-                case DIRECTION.DOWN:
-                    head.y++;
-                    break;
-                case DIRECTION.LEFT:
-                    head.x--;
-                    break;
-                case DIRECTION.RIGHT:
-                    head.x++;
-                    break;
-                default:
-                    break;
-            }
+                const head = { ...snakeParts[0] };
+                switch (direction) {
+                    case DIRECTION.UP:
+                        head.y--;
+                        break;
+                    case DIRECTION.DOWN:
+                        head.y++;
+                        break;
+                    case DIRECTION.LEFT:
+                        head.x--;
+                        break;
+                    case DIRECTION.RIGHT:
+                        head.x++;
+                        break;
+                    default:
+                        break;
+                }
 
-            const updatedSnake = [head, ...snakeParts.slice(0, -1)];
-            setSnakeParts(updatedSnake);
-        }, GAME_SPEED)
-        return () => { console.log('clear'); clearInterval(interval); }
+                const updatedSnake = [head, ...snakeParts.slice(0, -1)];
+                setSnakeParts(updatedSnake);
+            }
+        }, Math.max(GAME_SPEED - score, 30))
+        return () => { clearInterval(interval); }
     }, [direction, snakeParts])
 
     useEffect(() => {
@@ -106,17 +122,17 @@ export const Game = () => {
         }
 
         snakeParts.forEach((part) => {
-            if (direction === DIRECTION.UP && part.y < 0) {
-                part.y = Math.floor(window.innerHeight / 50);
+            if (direction === DIRECTION.UP && part.y < 3) {
+                part.y = Math.floor(window.innerHeight / 50) - 3;
             }
-            else if (direction === DIRECTION.DOWN && (part.y * 50) >  window.innerHeight) {
-                part.y = 0;
+            else if (direction === DIRECTION.DOWN && ((part.y + 3) * 50) > window.innerHeight) {
+                part.y = 2;
             }
-            else if (direction === DIRECTION.RIGHT && (part.x * 50) > window.innerWidth) {
-                part.x = 0
+            else if (direction === DIRECTION.RIGHT && ((part.x + 4) * 50) > window.innerWidth) {
+                part.x = 2;
             }
-            else if (direction === DIRECTION.LEFT && (part.x < 0)) {
-                part.x = Math.floor(window.innerWidth / 50);
+            else if (direction === DIRECTION.LEFT && (part.x < 3)) {
+                part.x = Math.floor(window.innerWidth / 50) - 3;
             }
         })
     }, [bottom, left])
@@ -132,7 +148,7 @@ export const Game = () => {
 
     const setRandomFoodPosition = () => {
         let foodPositionIsInSnake = true;
-        let newFoodPosition = { x: 0, y: 0};
+        let newFoodPosition = { x: 0, y: 0 };
         while (foodPositionIsInSnake) {
             foodPositionIsInSnake = false;
             newFoodPosition = generateRandomPosition();
@@ -145,30 +161,28 @@ export const Game = () => {
         setFood(newFoodPosition);
     }
 
-    const isDuplicatePart = (item:SnakePart, arr:SnakePart[]) => { 
-        return snakeParts.some((el:SnakePart) => item.x === el.x && item.y === el.y);
-    } 
+    const isDuplicatePart = (item: SnakePart, arr: SnakePart[]) => {
+        return arr.some((el: SnakePart) => item.x === el.x && item.y === el.y);
+    }
 
     const isSnakeSelfCollide = () => {
         const uniqueSnakeParts: SnakePart[] = [];
         snakeParts.forEach((part) => {
-            if (!isDuplicatePart(part, snakeParts)) {
+            if (!isDuplicatePart(part, uniqueSnakeParts)) {
                 uniqueSnakeParts.push(part);
             }
-        })
-        console.log(uniqueSnakeParts)
-        console.log(snakeParts);
+        });
         return uniqueSnakeParts.length < snakeParts.length;
     }
 
     const checkCollision = () => {
-        // console.log(`x: ${snakeParts[0].x}, y: ${snakeParts[0].y}`);   
         if (isSnakeSelfCollide()) {
-            console.log('self collide!');
+            setGameOver(true);
         }
-        if (
+        else if (
             snakeParts[0].x === food.x && snakeParts[0].y === food.y
         ) {
+            setScore((score) => score + 10);
             const head = { ...snakeParts[0] };
             switch (direction) {
                 case DIRECTION.UP:
@@ -191,16 +205,20 @@ export const Game = () => {
         }
     }
     return (
-        <div tabIndex={0} onKeyDown={keyDownHandler}>
-            <Food $left={food.x} $top={food.y} />
+        <>
+            {gameOver && <GameOver>GAME OVER!</GameOver>}
+            <ScoreBoard>{`SCORE: ${score}`}</ScoreBoard>
+            <div tabIndex={0} onKeyDown={keyDownHandler}>
+                <Food $left={food.x} $top={food.y} />
 
-            {snakeParts.map((part, index) => 
-                <SnakePart 
-                    part={part}
-                    key={index}
+                {snakeParts.map((part, index) =>
+                    <SnakePart
+                        part={part}
+                        key={index}
 
-                />
-            )}
-        </div>
+                    />
+                )}
+            </div>
+        </>
     )
 };
